5f9f079100dff35ad3241f8380064ab7f68465b7
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-06-10 00:44:03 -0700

    Now only compatible with Xcode 7-beta.

--- compatibility-update.sh
+++ compatibility-update.sh
@@ -1,3 +1,3 @@
-XcodeUUID=$(defaults read /Applications/Xcode.app/Contents/Info DVTPlugInCompatibilityUUID)
-defaults write ./Multiplex/Multiplex-Info.plist "DVTPlugInCompatibilityUUIDs" -array-add $XcodeUUID
+XcodeUUID=$(defaults read /Applications/Xcode-beta.app/Contents/Info DVTPlugInCompatibilityUUID)
+defaults write Multiplex/Multiplex-Info.plist "DVTPlugInCompatibilityUUIDs" -array-add $XcodeUUID
 echo "Added $XcodeUUID"
