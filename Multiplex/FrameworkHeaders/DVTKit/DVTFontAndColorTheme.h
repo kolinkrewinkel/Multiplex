9b7ba85739aaab1b1f526b2d08c2723718f0c30b
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-08-23 00:20:08 -0700

    Fix up projects

--- Multiplex/FrameworkHeaders/DVTKit/DVTFontAndColorTheme.h
+++ Multiplex/FrameworkHeaders/DVTKit/DVTFontAndColorTheme.h
@@ -4,6 +4,9 @@
 //     class-dump is Copyright (C) 1997-1998, 2000-2001, 2004-2013 by Steve Nygard.
 //
 
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wall"
+
 #import "CDStructures.h"
 
 #import "DVTPreferenceSet-Protocol.h"
@@ -216,3 +219,4 @@
 
 @end
 
+#pragma clang diagnostic pop
