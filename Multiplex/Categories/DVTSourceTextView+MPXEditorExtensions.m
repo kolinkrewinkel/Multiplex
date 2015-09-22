0bc6c3a7e825a0e5718a603294585bcc550d338c
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-09-22 10:51:42 -0700

    Update copyright years.

--- Multiplex/Categories/DVTSourceTextView+MPXEditorExtensions.m
+++ Multiplex/Categories/DVTSourceTextView+MPXEditorExtensions.m
@@ -3,7 +3,7 @@
 //  Multiplex
 //
 //  Created by Kolin Krewinkel on 9/25/14.
-//  Copyright (c) 2014 Kolin Krewinkel. All rights reserved.
+//  Copyright (c) 2015 Kolin Krewinkel. All rights reserved.
 //
 
 @import MPXFoundation;
--- Multiplex/Modules/MPXSelectionCore/MPXSelectionCore/MPXSelection.h
+++ Multiplex/Modules/MPXSelectionCore/MPXSelectionCore/MPXSelection.h
@@ -3,7 +3,7 @@
 //  Multiplex
 //
 //  Created by Kolin Krewinkel on 12/27/14.
-//  Copyright (c) 2014 Kolin Krewinkel. All rights reserved.
+//  Copyright (c) 2015 Kolin Krewinkel. All rights reserved.
 //
 
 static const NSUInteger MPXNoStoredLineIndex = NSUIntegerMax;
