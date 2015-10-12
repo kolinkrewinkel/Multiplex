8b2690b1be9081761ca300d00f5d4fa31b110839
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-10-11 17:24:47 -0700

    Fix more garbage headers

--- Multiplex/Modules/IDEHeaders/IDEHeaders/DVTFoundation/CDStructures.h
+++ Multiplex/Modules/IDEHeaders/IDEHeaders/DVTFoundation/CDStructures.h
@@ -20,21 +20,6 @@ struct ArrayBuilder {
     unsigned int _field4;
 };
 
-struct CGPoint {
-    double _field1;
-    double _field2;
-};
-
-struct CGRect {
-    struct CGPoint _field1;
-    struct CGSize _field2;
-};
-
-struct CGSize {
-    double _field1;
-    double _field2;
-};
-
 struct DVTBitSet {
     unsigned long long _field1;
     unsigned long long _field2[0];
@@ -102,41 +87,6 @@ struct _DVTDiffToken {
     int type;
 };
 
-struct _NSRange {
-    unsigned long long location;
-    unsigned long long length;
-};
-
-struct __hash_node<std::__1::__hash_value_type<DVTFuture *, long>, void *>;
-
-struct __sFILE {
-    char *_field1;
-    int _field2;
-    int _field3;
-    short _field4;
-    short _field5;
-    struct __sbuf _field6;
-    int _field7;
-    void *_field8;
-    CDUnknownFunctionPointerType _field9;
-    CDUnknownFunctionPointerType _field10;
-    CDUnknownFunctionPointerType _field11;
-    CDUnknownFunctionPointerType _field12;
-    struct __sbuf _field13;
-    struct __sFILEX *_field14;
-    int _field15;
-    unsigned char _field16[3];
-    unsigned char _field17[1];
-    struct __sbuf _field18;
-    int _field19;
-    long long _field20;
-};
-
-struct __sbuf {
-    char *_field1;
-    int _field2;
-};
-
 struct __va_list_tag {
     unsigned int _field1;
     unsigned int _field2;
@@ -148,113 +98,6 @@ struct fastsimplearray {
     unsigned long long _field1;
     unsigned long long _field2;
     void **_field3;
-};
-
-struct fsid {
-    int _field1[2];
-};
-
-struct sockaddr {
-    unsigned char _field1;
-    unsigned char _field2;
-    char _field3[14];
-};
-
-struct sockaddr_storage {
-    unsigned char ss_len;
-    unsigned char ss_family;
-    char __ss_pad1[6];
-    long long __ss_align;
-    char __ss_pad2[112];
-};
-
-struct stat {
-    int _field1;
-    unsigned short _field2;
-    unsigned short _field3;
-    unsigned long long _field4;
-    unsigned int _field5;
-    unsigned int _field6;
-    int _field7;
-    struct timespec _field8;
-    struct timespec _field9;
-    struct timespec _field10;
-    struct timespec _field11;
-    long long _field12;
-    long long _field13;
-    int _field14;
-    unsigned int _field15;
-    unsigned int _field16;
-    int _field17;
-    long long _field18[2];
-};
-
-struct statfs {
-    unsigned int _field1;
-    int _field2;
-    unsigned long long _field3;
-    unsigned long long _field4;
-    unsigned long long _field5;
-    unsigned long long _field6;
-    unsigned long long _field7;
-    struct fsid _field8;
-    unsigned int _field9;
-    unsigned int _field10;
-    unsigned int _field11;
-    unsigned int _field12;
-    char _field13[16];
-    char _field14[1024];
-    char _field15[1024];
-    unsigned int _field16[8];
-};
-
-struct timespec {
-    long long _field1;
-    long long _field2;
-};
-
-struct unique_ptr {
-    struct __compressed_pair {
-        struct __hash_node **__first_;
-        struct __bucket_list_deallocator {
-            struct __compressed_pair {
-                unsigned long long __first_;
-            } __data_;
-        } __second_;
-    } __ptr_;
-};
-
-struct unordered_map {
-    struct __hash_table {
-        struct unique_ptr __bucket_list_;
-        struct __compressed_pair {
-            struct __hash_node_base {
-                struct __hash_node *__next_;
-            } __first_;
-        } __p1_;
-        struct __compressed_pair {
-            unsigned long long __first_;
-        } __p2_;
-        struct __compressed_pair {
-            float __first_;
-        } __p3_;
-    } __table_;
-};
-
-struct vector {
-    dispatch_block_t *__begin_;
-    dispatch_block_t *__end_;
-    struct __compressed_pair {
-        dispatch_block_t *__first_;
-    } __end_cap_;
-};
-
-struct vector {
-    dispatch_block_t *__begin_;
-    dispatch_block_t *__end_;
-    struct __compressed_pair {
-        dispatch_block_t *__first_;
-    } __end_cap_;
 };
 
 #pragma mark Typedef'd Structures
