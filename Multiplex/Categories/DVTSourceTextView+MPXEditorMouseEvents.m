928ed19e92af52010a63edba893d97d5d684d351
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-10-12 10:09:11 -0700

    Reorder the methods so they follow the chronological order of mouse events.
    Also, cleanup the hover-swapping method.

--- Multiplex/Categories/DVTSourceTextView+MPXEditorMouseEvents.m
+++ Multiplex/Categories/DVTSourceTextView+MPXEditorMouseEvents.m
@@ -24,7 +24,83 @@
 @synthesizeAssociation(DVTSourceTextView, mpx_altPopoverTimer);
 @synthesizeAssociation(DVTSourceTextView, mpx_rangeInProgress);
 
-#pragma mark - Mouse Events
+- (void)mpx_mouseDown:(NSEvent *)theEvent
+{
+    NSUInteger index = ({
+        CGPoint clickLocation = [self convertPoint:[theEvent locationInWindow]
+                                          fromView:nil];
+        [self characterIndexForInsertionAtPoint:clickLocation];
+    });
+    
+    if (index == NSNotFound) {
+        return;
+    }
+    
+    BOOL altKeyHeld = (theEvent.modifierFlags & NSAlternateKeyMask) != 0;
+    if (altKeyHeld) {      
+        
+        self.mpx_altPopoverTimer = [NSTimer timerWithTimeInterval:1.0
+                                                           target:self
+                                                         selector:@selector(mpx_showAltPopover:)
+                                                         userInfo:RACTuplePack(@(index), theEvent)
+                                                          repeats:NO];
+        [[NSRunLoop mainRunLoop] addTimer:self.mpx_altPopoverTimer forMode:NSDefaultRunLoopMode];
+        
+        return;
+    }
+    
+    NSInteger clickCount = theEvent.clickCount;
+    NSRange resultRange = NSMakeRange(NSNotFound, 0);
+    
+    switch (clickCount) {
+        case 1:
+            // Selects only the single point at the approximate location of the cursor           
+            resultRange = NSMakeRange(index, 0);
+            break;
+        case 2: {
+            if ([self.layoutManager.foldingManager firstFoldTouchingCharacterIndex:index]) {
+                [self mpx_mouseDown:theEvent];
+                return;
+            }
+            
+            resultRange = [self.textStorage doubleClickAtIndex:index];
+            break;
+        }
+        case 3:
+            resultRange = [self.textStorage.string lineRangeForRange:NSMakeRange(index, 0)];
+            break;
+        default:
+            return;
+    }
+    
+    if (resultRange.location == NSNotFound) {
+        return;
+    }
+    
+    [self.mpx_textViewSelectionDecorator stopBlinking];
+    [self.mpx_textViewSelectionDecorator setCursorsVisible:YES];
+    
+    MPXSelection *selection = [[MPXSelection alloc] initWithSelectionRange:resultRange
+                                                     indexWantedWithinLine:MPXNoStoredLineIndex
+                                                                    origin:resultRange.location];
+    self.mpx_rangeInProgress = selection;
+    
+    BOOL commandKeyHeld = (theEvent.modifierFlags & NSCommandKeyMask) != 0;
+    if (commandKeyHeld) {
+        NSArray *selections = self.mpx_selectionManager.visualSelections;
+        [self.mpx_selectionManager setTemporarySelections:[selections arrayByAddingObject:selection]];
+    } else {
+        self.mpx_shouldCloseGroupOnNextChange = [self.mpx_selectionManager.finalizedSelections count] > 0;
+        
+        // Because the click was singular, the other selections will *not* come back under any circumstances.
+        // Thus, it must be finalized at the point where it's at is if it's a zero-length selection.
+        // Otherwise, they'll be re-added during dragging.
+        self.mpx_selectionManager.finalizedSelections = @[selection];
+        
+        // In the event the user drags, however, it needs to unfinalized so that it can be extended again.
+        [self.mpx_selectionManager setTemporarySelections:@[selection]];
+    }
+}
 
 - (void)mpx_mouseDragged:(NSEvent *)theEvent
 {
@@ -61,97 +137,6 @@
     [self autoscroll:theEvent];
 }
 
-- (void)mpx_showAltPopover:(NSTimer *)sender
-{    
-    RACTuple *tuple = sender.userInfo;
-    RACTupleUnpack(NSNumber *nIndex, NSEvent *event) = tuple;
-    
-    [((id<DVTSourceTextViewDelegate>)self.delegate) textView:self
-                     didClickOnTemporaryLinkAtCharacterIndex:nIndex.unsignedIntegerValue
-                                                       event:event
-                                                  isAltEvent:YES];
-    
-    [sender invalidate];
-}
-
-- (void)mpx_mouseDown:(NSEvent *)theEvent
-{    
-    NSUInteger index = ({
-        CGPoint clickLocation = [self convertPoint:[theEvent locationInWindow]
-                                          fromView:nil];
-        [self characterIndexForInsertionAtPoint:clickLocation];
-    });
-
-    if (index == NSNotFound) {
-        return;
-    }
-    
-    BOOL altKeyHeld = (theEvent.modifierFlags & NSAlternateKeyMask) != 0;
-    if (altKeyHeld) {      
-        
-        self.mpx_altPopoverTimer = [NSTimer timerWithTimeInterval:1.0
-                                                           target:self
-                                                         selector:@selector(mpx_showAltPopover:)
-                                                         userInfo:RACTuplePack(@(index), theEvent)
-                                                          repeats:NO];
-        [[NSRunLoop mainRunLoop] addTimer:self.mpx_altPopoverTimer forMode:NSDefaultRunLoopMode];
-        
-        return;
-    }
-
-    NSInteger clickCount = theEvent.clickCount;
-    NSRange resultRange = NSMakeRange(NSNotFound, 0);
-    
-    switch (clickCount) {
-        case 1:
-            // Selects only the single point at the approximate location of the cursor           
-            resultRange = NSMakeRange(index, 0);
-            break;
-        case 2: {
-            if ([self.layoutManager.foldingManager firstFoldTouchingCharacterIndex:index]) {
-                [self mpx_mouseDown:theEvent];
-                return;
-            }
-            
-            resultRange = [self.textStorage doubleClickAtIndex:index];
-            break;
-        }
-        case 3:
-            resultRange = [self.textStorage.string lineRangeForRange:NSMakeRange(index, 0)];
-            break;
-        default:
-            return;
-    }
-
-    if (resultRange.location == NSNotFound) {
-        return;
-    }
-
-    [self.mpx_textViewSelectionDecorator stopBlinking];
-    [self.mpx_textViewSelectionDecorator setCursorsVisible:YES];
-
-    MPXSelection *selection = [[MPXSelection alloc] initWithSelectionRange:resultRange
-                                                     indexWantedWithinLine:MPXNoStoredLineIndex
-                                                                    origin:resultRange.location];
-    self.mpx_rangeInProgress = selection;
-
-    BOOL commandKeyHeld = (theEvent.modifierFlags & NSCommandKeyMask) != 0;
-    if (commandKeyHeld) {
-        NSArray *selections = self.mpx_selectionManager.visualSelections;
-        [self.mpx_selectionManager setTemporarySelections:[selections arrayByAddingObject:selection]];
-    } else {
-        self.mpx_shouldCloseGroupOnNextChange = [self.mpx_selectionManager.finalizedSelections count] > 0;
-
-        // Because the click was singular, the other selections will *not* come back under any circumstances.
-        // Thus, it must be finalized at the point where it's at is if it's a zero-length selection.
-        // Otherwise, they'll be re-added during dragging.
-        self.mpx_selectionManager.finalizedSelections = @[selection];
-
-        // In the event the user drags, however, it needs to unfinalized so that it can be extended again.
-        [self.mpx_selectionManager setTemporarySelections:@[selection]];
-    }
-}
-
 - (void)mpx_mouseUp:(NSEvent *)theEvent
 {
     BOOL showedAltPopover = !self.mpx_altPopoverTimer.valid;
@@ -186,47 +171,73 @@
         
         return;
     }
-  
+    
     self.mpx_selectionManager.finalizedSelections = self.mpx_selectionManager.visualSelections;
     self.mpx_rangeInProgress = nil;
     
     [self.mpx_textViewSelectionDecorator startBlinking];
 }
 
-- (void)mpx_updateTemporaryLinkUnderMouseForEvent:(NSEvent *)event
-{
-    BOOL altKeyHeld = (event.modifierFlags & NSAlternateKeyMask) != 0;
-    BOOL commandKeyHeld = (event.modifierFlags & NSCommandKeyMask) != 0;
-    if (!commandKeyHeld) {        
-        NSEvent *substituteEvent = event;
-        
-        if (altKeyHeld) {
-            if (event.type == NSMouseMoved) {
-                substituteEvent = [NSEvent mouseEventWithType:event.type
-                                                     location:event.locationInWindow
-                                                modifierFlags:NSCommandKeyMask
-                                                    timestamp:event.timestamp
-                                                 windowNumber:event.windowNumber
-                                                      context:event.context
-                                                  eventNumber:event.eventNumber
-                                                   clickCount:event.clickCount
-                                                     pressure:event.pressure];
-            } else if (event.type == NSFlagsChanged) {
-                substituteEvent = [NSEvent keyEventWithType:event.type
-                                                   location:event.locationInWindow
-                                              modifierFlags:NSCommandKeyMask
-                                                  timestamp:event.timestamp
-                                               windowNumber:event.windowNumber
-                                                    context:event.context
-                                                 characters:@""
-                                charactersIgnoringModifiers:@""
-                                                  isARepeat:NO
-                                                    keyCode:event.keyCode];
-            }
-        }
+#pragma mark - Long-press definition handler
 
-        [self mpx_updateTemporaryLinkUnderMouseForEvent:substituteEvent];
+- (void)mpx_showAltPopover:(NSTimer *)sender
+{
+    RACTuple *tuple = sender.userInfo;
+    RACTupleUnpack(NSNumber *nIndex, NSEvent *event) = tuple;
+    
+    [((id<DVTSourceTextViewDelegate>)self.delegate) textView:self
+                     didClickOnTemporaryLinkAtCharacterIndex:nIndex.unsignedIntegerValue
+                                                       event:event
+                                                  isAltEvent:YES];
+    
+    [sender invalidate];
+}
+
+/**
+ * Flips the modifier flags so that Alt shows as Command, and command isn't sent altogether so no solid underline is
+ * shown.
+ */
+ - (void)mpx_updateTemporaryLinkUnderMouseForEvent:(NSEvent *)event
+{
+    // If the command key is held, do not show any form of temporary link. The I-beam should be shown instead so users
+    // know 
+    BOOL commandKeyHeld = (event.modifierFlags & NSCommandKeyMask) != 0;    
+    if (commandKeyHeld) {
+        return;
     }
+    
+    // Swap the event if the alt-key is held so it looks like the command key is being held.
+    // Not exactly an elegant fix, but it requires the least-specific knowledge (and messing with) the internals of
+    // the text view.
+    NSEvent *substituteEvent = event;
+    
+    BOOL altKeyHeld = (event.modifierFlags & NSAlternateKeyMask) != 0;     
+    if (altKeyHeld) {
+        if (event.type == NSMouseMoved) {
+            substituteEvent = [NSEvent mouseEventWithType:event.type
+                                                 location:event.locationInWindow
+                                            modifierFlags:NSCommandKeyMask
+                                                timestamp:event.timestamp
+                                             windowNumber:event.windowNumber
+                                                  context:event.context
+                                              eventNumber:event.eventNumber
+                                               clickCount:event.clickCount
+                                                 pressure:event.pressure];
+        } else if (event.type == NSFlagsChanged) {
+            substituteEvent = [NSEvent keyEventWithType:event.type
+                                               location:event.locationInWindow
+                                          modifierFlags:NSCommandKeyMask
+                                              timestamp:event.timestamp
+                                           windowNumber:event.windowNumber
+                                                context:event.context
+                                             characters:@""
+                            charactersIgnoringModifiers:@""
+                                              isARepeat:NO
+                                                keyCode:event.keyCode];
+        }
+    }
+
+    [self mpx_updateTemporaryLinkUnderMouseForEvent:substituteEvent];
 }
 
 @end
