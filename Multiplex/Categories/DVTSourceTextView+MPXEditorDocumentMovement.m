803189f6dcc7138cccf56d2d86f731010d4702ff
Kolin Krewinkel <kolin@kolinkrewinkel.com>
2015-10-23 22:55:52 -0700

    Fix #20: Rewrite document movement (⌘-⇧-↓ and ⌘-⇧-↑) to use new mapping method, not mess up selection's origin

--- Multiplex/Categories/DVTSourceTextView+MPXEditorDocumentMovement.m
+++ Multiplex/Categories/DVTSourceTextView+MPXEditorDocumentMovement.m
@@ -16,54 +16,80 @@
 
 @implementation DVTSourceTextView (MPXEditorDocumentMovement)
 
-#pragma mark - Logic
-
-- (void)mpx_moveToRange:(NSRange)newRange modifyingSelection:(BOOL)modifySelection
-{
-    [self mpx_mapAndFinalizeSelectedRanges:^MPXSelection *(MPXSelection *selection) {
-        NSRange previousAbsoluteRange = selection.range;
-        NSRange newAbsoluteRange = newRange;
-
-        if (modifySelection) {
-            newAbsoluteRange = NSUnionRange(previousAbsoluteRange, newAbsoluteRange);
-        }
-
-        return [[MPXSelection alloc] initWithSelectionRange:newAbsoluteRange
-                                      indexWantedWithinLine:MPXNoStoredLineIndex
-                                                     origin:selection.insertionIndex];
-    }];
-}
-
-- (void)mpx_moveToBeginningOfDocumentModifyingSelection:(BOOL)modifySelection
-{
-    [self mpx_moveToRange:NSMakeRange(0, 0) modifyingSelection:modifySelection];
-}
-
-- (void)mpx_moveToEndOfDocumentModifyingSelection:(BOOL)modifySelection
-{
-    [self mpx_moveToRange:NSMakeRange([self.textStorage length] - 1, 0) modifyingSelection:modifySelection];
-}
-
-#pragma mark - Forwarding Methods
-
 - (void)moveToBeginningOfDocument:(id)sender
 {
-    [self mpx_moveToBeginningOfDocumentModifyingSelection:NO];
+    // This is a pretty straightforward mapping back to 0,0.
+    MPXSelectionMutationBlock transformBlock = ^MPXSelectionMutation *(MPXSelection *selectionToModify) {
+        MPXSelection *newSelection = [[MPXSelection alloc] initWithSelectionRange:NSMakeRange(0, 0)
+                                                            indexWantedWithinLine:MPXNoStoredLineIndex
+                                                                           origin:0];
+
+        return [[MPXSelectionMutation alloc] initWithInitialSelection:selectionToModify
+                                                       finalSelection:newSelection
+                                                          mutatedText:NO];
+    };
+
+    [self.mpx_selectionManager mapSelectionsWithMovementDirection:NSSelectionAffinityUpstream
+                                              modifyingSelections:NO
+                                                       usingBlock:transformBlock];
 }
 
 - (void)moveToBeginningOfDocumentAndModifySelection:(id)sender
 {
-    [self mpx_moveToBeginningOfDocumentModifyingSelection:YES];
+    MPXSelectionMutationBlock transformBlock = ^MPXSelectionMutation *(MPXSelection *selectionToModify) {
+        // Moving back to 0, we can just decrement by the location of the caret; set the origin to the trailing edge of
+        // the selection.
+        NSRange newRange = [selectionToModify modifySelectionUpstreamByAmount:selectionToModify.insertionIndex];
+        MPXSelection *newSelection = [[MPXSelection alloc] initWithSelectionRange:newRange
+                                                            indexWantedWithinLine:MPXNoStoredLineIndex
+                                                                           origin:NSMaxRange(newRange)];
+
+        return [[MPXSelectionMutation alloc] initWithInitialSelection:selectionToModify
+                                                       finalSelection:newSelection
+                                                          mutatedText:NO];
+    };
+
+    [self.mpx_selectionManager mapSelectionsWithMovementDirection:NSSelectionAffinityUpstream
+                                              modifyingSelections:YES
+                                                       usingBlock:transformBlock];
 }
 
 - (void)moveToEndOfDocument:(id)sender
 {
-    [self mpx_moveToEndOfDocumentModifyingSelection:NO];
+    MPXSelectionMutationBlock transformBlock = ^MPXSelectionMutation *(MPXSelection *selectionToModify) {
+        NSUInteger endOfDocument = [self.textStorage.string length];
+        MPXSelection *newSelection = [[MPXSelection alloc] initWithSelectionRange:NSMakeRange(endOfDocument, 0)
+                                                            indexWantedWithinLine:MPXNoStoredLineIndex
+                                                                           origin:endOfDocument];
+
+        return [[MPXSelectionMutation alloc] initWithInitialSelection:selectionToModify
+                                                       finalSelection:newSelection
+                                                          mutatedText:NO];
+    };
+
+    [self.mpx_selectionManager mapSelectionsWithMovementDirection:NSSelectionAffinityDownstream
+                                              modifyingSelections:NO
+                                                       usingBlock:transformBlock];
 }
 
 - (void)moveToEndOfDocumentAndModifySelection:(id)sender
 {
-    [self mpx_moveToEndOfDocumentModifyingSelection:YES];
+    MPXSelectionMutationBlock transformBlock = ^MPXSelectionMutation *(MPXSelection *selectionToModify) {
+        NSUInteger deltaToEnd = [self.textStorage.string length] - selectionToModify.insertionIndex;
+        NSRange newRange = [selectionToModify modifySelectionDownstreamByAmount:deltaToEnd];
+
+        MPXSelection *newSelection = [[MPXSelection alloc] initWithSelectionRange:newRange
+                                                            indexWantedWithinLine:MPXNoStoredLineIndex
+                                                                           origin:newRange.location];
+
+        return [[MPXSelectionMutation alloc] initWithInitialSelection:selectionToModify
+                                                       finalSelection:newSelection
+                                                          mutatedText:NO];
+    };
+
+    [self.mpx_selectionManager mapSelectionsWithMovementDirection:NSSelectionAffinityDownstream
+                                              modifyingSelections:YES
+                                                       usingBlock:transformBlock];
 }
 
 @end
